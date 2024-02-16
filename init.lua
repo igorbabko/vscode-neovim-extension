7085bfec6658f4d10c9271453a5aeb0579070a06
Igor Babko <i.i.babko@gmail.com>
2024-02-16 11:49:05 +0200

    03. Motion keys

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- motion keys (left, down, up, right)
+vim.keymap.set({ 'n', 'v' }, 'j', 'h')
+vim.keymap.set({ 'n', 'v' }, 'k', 'j')
+vim.keymap.set({ 'n', 'v' }, 'l', 'k')
+vim.keymap.set({ 'n', 'v' }, ';', 'l')
