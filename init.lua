917fbe95b142cdc7d7e168aa8ee574598b9d4a49
Igor Babko <i.i.babko@gmail.com>
2024-02-16 12:14:50 +0200

    04. Remaps

--- init.lua
+++ init.lua
@@ -3,3 +3,15 @@ vim.keymap.set({ 'n', 'v' }, 'j', 'h')
 vim.keymap.set({ 'n', 'v' }, 'k', 'j')
 vim.keymap.set({ 'n', 'v' }, 'l', 'k')
 vim.keymap.set({ 'n', 'v' }, ';', 'l')
+
+-- repeat previous f, t, F or T movement
+vim.keymap.set('n', '\'', ';')
+
+-- paste without overwriting
+vim.keymap.set('v', 'p', 'P')
+
+-- redo
+vim.keymap.set('n', 'U', '<C-r>')
+
+-- clear search highlighting
+vim.keymap.set('n', '<Esc>', ':nohlsearch<cr>')
